492d2ac950beadf9f7255c108348b426e585e3b7
Martin Sanne Kristiansen <martin@heydays.no>
2020-01-08 09:37:38 +0100

    :boom:

---
+++ heydays-test-plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:       Heydays Test Plugin
+ * Plugin URI:        https://github.com/heydaysoslo/heydays-test-plugin
+ * Description:       A plugin to test automatic updates through GitHub
+ * Version:           0.0.1
+ * Author:            Martin Sanne Kristansen
+ * Text Domain:       heydays-test-plugin
+ * Network:           true
+ * GitHub Plugin URI: https://github.com/heydaysoslo/heydays-test-plugin
+ */
+
+/*
+ * Exit if called directly.
+ * PHP version check and exit.
+ */
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
